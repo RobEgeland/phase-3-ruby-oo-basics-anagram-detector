04216c6dc6ef90d69a30d178bdbe5d355f1d58c2
Robert Egeland <robegeland@yahoo.com>
2022-05-12 14:47:45 -0500

    commit tree 9b43e71f3f7a8a0bbc8f23307b2f76f939c64b67

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        return_array = []
+        array.each do |word|
+            split = word.chars
+            if split.sort == @word.chars.sort
+                return_array << word
+            end
+        end
+        return_array
+    end
+
+end
